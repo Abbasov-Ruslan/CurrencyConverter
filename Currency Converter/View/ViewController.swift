2d255a7105a9829989cf718c25e5611f9c0f4d51
Ruslan Abbasov <abbasov.ruslan1999@yndex.ru>
2022-09-03 12:16:43 +0700

    add func for rightTextfield

--- Currency Converter/View/ViewController.swift
+++ Currency Converter/View/ViewController.swift
@@ -11,23 +11,46 @@ import Combine
 class ViewController: UIViewController {
 
     
-    @IBOutlet weak var leftNumberField: UITextField!
-    @IBOutlet weak var rightNumberField: UITextField!
+    @IBOutlet private weak var updateButton: UIButton!
+    @IBOutlet private weak var updateTimeButton: UIButton!
+    @IBOutlet private weak var changeButton: UIButton!
+    @IBOutlet private weak var leftNumberField: UITextField!
+    @IBOutlet private weak var rightNumberField: UITextField!
     
     var viewModel = CurrencyConverterViewModel()
     var roubleToUsdRate: Double = 0
+    var usdToRoubleRate: Double = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupButton(button: updateButton)
+        setupButton(button: updateTimeButton)
+        setupButton(button: changeButton)
     }
 
     @IBAction func leftTextFieldChanged(_ sender: Any) {
+        roubleToUsdRate = viewModel.getCurrentRoubleToUSDRate()
         let leftText: String? = leftNumberField.text
         guard let leftNumber = Double(leftText ?? "") else {
             return
         }
         rightNumberField.text = String(leftNumber * roubleToUsdRate)
     }
-    
+
+    @IBAction func rightTextFieldChange(_ sender: Any) {
+        roubleToUsdRate = viewModel.getCurrentUsdToRoubleRate()
+        let rightText: String? = rightNumberField.text
+        guard let rightNumber = Double(rightText ?? "") else {
+            return
+        }
+        leftNumberField.text = String(rightNumber * roubleToUsdRate)
+    }
+
+    private func setupButton(button: UIButton) {
+        button.layer.cornerRadius = 5
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.black.cgColor
+    }
+
 }
 
